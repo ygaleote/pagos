1eb586fc0760f931599eb5191e287954fcbcab16
Yordi Angelo <angelox9402@gmail.com>
2022-06-20 13:27:22 -0500

    inicial

---
+++ src/test/java/com/softevolucion/pagos/PagosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.softevolucion.pagos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PagosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
